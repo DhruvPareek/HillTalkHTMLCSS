333846e7dfe0862656bf76b88725f46df56f7b2d
Ryan Miller <ryan.bmiller1892@gmail.com>
2023-02-16 13:18:35 -0800

    Added .js file DiningHalls.js
    Contains functions for sorting buttons, same as Dorms.js (different function names, of course)

---
+++ DiningHalls.js
@@ -0,0 +1,20 @@
+function sortDiningHealth() {
+    /*TODO*/
+    return;
+}
+    
+function sortDiningQuality() {
+    /*TODO*/
+    return;
+}
+    
+function sortDiningWait() {
+    /*TODO*/
+    return;
+}
+    
+function sortDiningHours() {
+    /*TODO*/
+    return;
+}
+        
