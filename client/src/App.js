3677a2db1dcde9896697f93be74f1a654901ab01
Aarya <aarya.tallada@gmail.com>
2023-02-24 12:37:42 -0800

    Added client folder for front end and server folder for back end
    Created a react app within client folder in order to start the database

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test for Reveiws</h1>
+      <div className="form">
+        <label>Olympic Hall Cleanliness</label>
+      <input type="text" name="Olympic Hall Cleanliness" />
+      <label></label>
+      <input type="text" name="Review" />
+      </div>   
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default App;
