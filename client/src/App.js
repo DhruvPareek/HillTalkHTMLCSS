59284efb7be8bbb6ebd6cdc9f4bd5ef300d70fff
Aarya <aarya.tallada@gmail.com>
2023-02-24 16:29:07 -0800

    Created React App that actually works
    Imported router so linking separate js files to App.js is possible

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,54 @@
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Test for Reveiws</h1>
-      <div className="form">
-        <label>Olympic Hall Cleanliness</label>
-      <input type="text" name="Olympic Hall Cleanliness" />
-      <label></label>
-      <input type="text" name="Review" />
-      </div>   
-      <button>Submit</button>
-    </div>
-  );
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Test for Reveiws</h1>
+//       <div className="form">
+//         <label>Olympic Hall Cleanliness</label>
+//       <input type="text" name="Olympic Hall Cleanliness" />
+//       <label></label>
+//       <input type="text" name="Review" />
+//       </div>   
+//       <button onclick='buttonclick()' >Submit</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+function App()
+{
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
 }
 
 export default App;
