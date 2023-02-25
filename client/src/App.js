5754dddb4601a1755539119adbf60ae0aec6deca
Lakshman Sundaram <lakshman.sun27@gmail.com>
2023-02-24 18:16:27 -0800

    Get multiple pages in React app up and running
    Added skeleton for react app with multiple pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,35 @@
-// import './App.css';
+// App.js
 
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Test for Reveiws</h1>
-//       <div className="form">
-//         <label>Olympic Hall Cleanliness</label>
-//       <input type="text" name="Olympic Hall Cleanliness" />
-//       <label></label>
-//       <input type="text" name="Review" />
-//       </div>   
-//       <button onclick='buttonclick()' >Submit</button>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Dorms from "./Dorms";
+import Contact from "./Contact";
 
-function App()
-{
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Dorms</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<Dorms />} />
+      <Route path="/contact" element={<Contact />} />
+      </Routes>
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link to="about">About Us</Link>
-      </div>
-    ),
-  },
-  {
-    path: "about",
-    element: <div>About</div>,
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+    </Router>
+  );
 }
 
 export default App;
