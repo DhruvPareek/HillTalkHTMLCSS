cf0f465d35a5a7d751423ae3b9f9d4830d3eaab4
Aarya <aarya.tallada@gmail.com>
2023-03-03 22:55:19 -0800

    fixed the accidentally overwrote home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,23 +1,21 @@
- 
- export default function Home() {
-    return (
-  <html>
-      <head>
-  <title>HillTalk</title>
-  <link rel="stylesheet" href="format.css" />
+
+export default function Home() {
+  return (
+<html>
+    <head>
+<title>HillTalk</title>
+<link rel="stylesheet" href="format.css" />
 </head>
 
 <body>
-  <script type="text/javascript" src="test.js"></script>
-  <section class="header">
-  </section>
-  <img src="http://www.housing.ucla.edu/maps/ochmap.jpg" alt="map" width="720" height="405" class = "map" />
-  <p class ="description">A Place To Rate, Review, and Discuss the Different Features of the Hill</p>
+<script type="text/javascript" src="test.js"></script>
+<section class="header">
+</section>
+<img src="http://www.housing.ucla.edu/maps/ochmap.jpg" alt="map" width="720" height="405" class = "map" />
+<p class ="description">A Place To Rate, Review, and Discuss the Different Features of the Hill</p>
 
 </body>
 </html>
 
-    );
-  }
-  
-  
+  );
+}
