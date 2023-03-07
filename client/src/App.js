766fa6b797e991996788ede979a01eb982e939be
Colin Hoggatt <colinhoggatt@gmail.com>
2023-03-07 11:37:59 -0800

    Not updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,37 +18,58 @@
 // }
 
 // export default App;
-import React from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+//   Link,
+// } from "react-router-dom";
 
-function App()
-{
+// function App()
+// {
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link to="about">About Us</Link>
-      </div>
-    ),
-  },
-  {
-    path: "about",
-    element: <div>About</div>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <div>
+//         <h1>Hello World</h1>
+//         <Link to="about">About Us</Link>
+//       </div>
+//     ),
+//   },
+//   {
+//     path: "about",
+//     element: <div>About</div>,
+//   },
+// ]);
 
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+// createRoot(document.getElementById("root")).render(
+//   <RouterProvider router={router} />
+// );
+// }
+
+// App.js
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import About from './About';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/About">
+          <About />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
