557f42863e850b8563b508b2bad3ba4e57a38f87
Aarya <aarya.tallada@gmail.com>
2023-03-07 14:51:40 -0800

    Added some authentication skeleton

--- client/src/Home.js
+++ client/src/Home.js
@@ -13,7 +13,26 @@ export default function Home() {
 </section>
 <img src="http://www.housing.ucla.edu/maps/ochmap.jpg" alt="map" width="720" height="405" class = "map" />
 <p class ="description">A Place To Rate, Review, and Discuss the Different Features of the Hill</p>
+<br></br>
+<div>
+  <p>Register User</p>
+  <input placeholder="Email..." />
+  <input placeholder="Password..."/>
 
+  <button>Create User</button>
+
+</div>
+
+<div>
+  <p>Login</p>
+  <input placeholder="Email..."/>
+  <input placeholder="Password..."/>
+
+  <button>Login</button>
+
+</div>
+
+<p>User Logged In: </p>
 </body>
 </html>
 
