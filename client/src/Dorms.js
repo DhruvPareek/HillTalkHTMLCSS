cd34fa91375eba470008644b5c721e36b0c4751e
DhruvP <dhruvpareek883@gmail.com>
2023-03-06 22:59:57 -0800

    Backend Barely Connected, Changed Every Class to Hooks
    22 minutes into connecting firebase video. Console.log works to see what's in database.
    I changed Dorms.js, RecCenters.js, and DiningHalls.js to be a function instead of class.

--- client/src/Dorms.js
+++ client/src/Dorms.js
@@ -1,108 +1,73 @@
 // About.js
 
+import {useState, useEffect} from "react";
 import React from "react";
-import './App.css';
+import "./App.css";
 
-class Dorms extends React.Component{
+import {db} from "./firebase-config"
+import {collection, getDocs} from "firebase/firestore";
 
-  constructor(props){
-  super(props);
-  this.state = {
-    sortedby: '',
-    writingReview: false}
-    /*use this writingReview bool to change whether the person is in the process of leaving a review
-    or they want to leave a review*/
-  this.clickedSort = this.clickedSort.bind(this)
-  }
-
-  /*This function gets called when someone hits the button, it will set the state of the writingReview 
-  to the opposite of whatever it is at the moment */
-  leaveReview(props)
-  {
-    if(this.state.writingReview == true){
-      this.setState({
-        writingReview: false
-      })
-    }else{
-      this.setState({
-        writingReview: true
-      })
-    }
-  }
-
-  clickedSort(props)
-  {
-      
-      if(props === 1)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Cleanliness') 
-          this.setState({sortedBy: 'Cleanliness'})
-      }
-      if(props === 2)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Quality');  
-          this.setState({sortedBy: 'Quality'})
-      }
-      if(props === 3)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Space');  
-          this.setState({sortedBy: 'Space'})
-      }
-      if(props === 4)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Location');  
-          this.setState({sortedBy: 'Location'})
-      }
-      else
-      {
-          this.setState({sortedBy: 'none'})
-      }
-  }
-
-render() {
-  return (
-<html>
-<head>
-<title>HillTalk</title>
-</head>
-<body>
-<img src="https://i.pinimg.com/736x/26/04/67/2604674112dd693949fa3cc6babe7c71--dorm-ideas-hall.jpg" alt="tripleClassic" width="720" height="405" class = "tripleClassic"/>
-  <p>Here you can find every dorm on campus.</p>
-   <p>Sort By:</p>
-   <ul>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(1);}}>Cleanliness{this.state.sortedby}</button></ol>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(2);}}>Quality{this.state.sortedby}</button></ol>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(3);}}>Space{this.state.sortedby}</button></ol>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(4);}}>Location{this.state.sortedby}</button></ol>
-    </ul>
-    <br></br>
-    <h3>Centennial/Olympic</h3>
-    <img src="https://s3.amazonaws.com/cms.ipressroom.com/173/files/20218/614102382cfac27232f4ea45_Olympic+and+Centennial+Hall_5DM47510_Ext2/Olympic+and+Centennial+Hall_5DM47510_Ext2_hero.jpg"  width="250" height="200" class="CentennialOlympic"></img>
-    {/*The below line is how the button works, it checks the state for the variable 'writingReview', if writingReview
-    is set to true, that means the person in the act of writing a review, so the button should say submit review and a text box should pop up. 
-    If the bool writingReview is false, the button should say "Leave a Review" to give the person the option to write
-    a review */}
-    <br />
-    <button type='button' className="ReviewButton" onClick={() => { this.leaveReview();}}>{this.state.writingReview ? "Submit Review" : "Leave a Review"}</button>
-    {/*The line below will render a text box by calling calling the function: renderTextBox()
-    if writingReview bool is true. Else is does nothing */}
-    <div class="TextBox">
-      {this.state.writingReview ? renderTextBox() : " "}
-    </div>
-</body>
-
-</html>
-  );
-}
-}
-
-//This function actually returns the text box
-function renderTextBox(){
+function Dorms(){
+  ReviewDatabase();
   return (
     <html>
-      <input type="text" name="review" />
+    <head>
+    <title>HillTalk</title>
+    </head>
+    <body>
+    <img src="https://i.pinimg.com/736x/26/04/67/2604674112dd693949fa3cc6babe7c71--dorm-ideas-hall.jpg" alt="tripleClassic" width="720" height="405" class = "tripleClassic"/>
+      <p>Here you can find every dorm on campus.</p>
+       <p>Sort By:</p>
+       <ul>
+        <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(1);}}>Cleanliness{}</button></ol>
+        <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(2);}}>Quality{}</button></ol>
+        <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(3);}}>Space{}</button></ol>
+        <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(4);}}>Location{}</button></ol>
+        </ul>
+        <br></br>
+        <h3>Centennial/Olympic</h3>
+        <img src="https://s3.amazonaws.com/cms.ipressroom.com/173/files/20218/614102382cfac27232f4ea45_Olympic+and+Centennial+Hall_5DM47510_Ext2/Olympic+and+Centennial+Hall_5DM47510_Ext2_hero.jpg"  width="250" height="200" class="CentennialOlympic"></img>
+        <br />
+    </body>
+    
     </html>
   );
+}
+
+function clickedSort(props)
+{
+    
+    if(props === 1)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Cleanliness') 
+    }
+    if(props === 2)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Quality');  
+    }
+    if(props === 3)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Space');  
+    }
+    if(props === 4)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Location');  
+    }
+}
+
+function ReviewDatabase(){
+  const [reviews, setReview] = useState([]);
+  const reviewCollectionRef = collection(db, "Reviews")
+
+  useEffect(() => {
+    
+    const getReviews = async () => {
+      const data = await getDocs(reviewCollectionRef);
+      console.log(data);
+    }
+
+    getReviews()
+  }, [])
 }
 
 export default Dorms;
--- client/src/RecCenters.js
+++ client/src/RecCenters.js
@@ -2,49 +2,9 @@
 import './App.css';
 import React from "react";
 
-class RecCenters extends React.Component{
-  constructor(props){
-  super(props);
-  this.state = {sortedby: ''}
-  this.clickedSort = this.clickedSort.bind(this)
-  }
-
-  clickedSort(props)
-  {     
-      if(props === 1)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Facility Quaity') 
-          this.setState({sortedBy: 'Facility Quaity'})
-      }
-      if(props === 2)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Hours');  
-          this.setState({sortedBy: 'Hours'})
-      }
-      if(props === 3)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Space');  
-          this.setState({sortedBy: 'Space'})
-      }
-      if(props === 4)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Location');  
-          this.setState({sortedBy: 'Location'})
-      }
-      if(props === 5)
-      {
-          alert('hello, this should show up if the page rendered lol, Sort by Business');  
-          this.setState({sortedBy: 'Business'})
-      }
-      else
-      {
-          this.setState({sortedBy: 'none'})
-      }
-  }
-
-render() {
-  return (
-<html>
+function RecCenters() {
+    return (
+        <html>
 <head>
 <title>HillTalk</title>
 </head>
@@ -53,16 +13,39 @@ render() {
   <p>Here you can find every dorm on campus.</p>
    <p>Sort By:</p>
    <ul>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(1);}}>Facility Quaity{this.state.sortedby}</button></ol>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(2);}}>Hours{this.state.sortedby}</button></ol>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(3);}}>Space{this.state.sortedby}</button></ol>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(4);}}>Location{this.state.sortedby}</button></ol>
-    <ol><button type='button' className="btn btn-primary" onClick={() => { this.clickedSort(4);}}>Business{this.state.sortedby}</button></ol>
+    <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(1);}}>Facility Quaity{}</button></ol>
+    <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(2);}}>Hours{}</button></ol>
+    <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(3);}}>Space{}</button></ol>
+    <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(4);}}>Location{}</button></ol>
+    <ol><button type='button' className="btn btn-primary" onClick={() => { clickedSort(4);}}>Business{}</button></ol>
     </ul>
 </body>
 </html>
-  );
+    );
 }
+
+function clickedSort(props)
+{     
+    if(props === 1)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Facility Quaity') 
+    }
+    if(props === 2)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Hours');  
+    }
+    if(props === 3)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Space');  
+    }
+    if(props === 4)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Location'); 
+    }
+    if(props === 5)
+    {
+        alert('hello, this should show up if the page rendered lol, Sort by Business');  
+    }
 }
 
 export default RecCenters;
