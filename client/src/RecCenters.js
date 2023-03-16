bba92bb255b734162f9c56f37ea36cf7ac229ba3
DhruvP <dhruvpareek883@gmail.com>
2023-03-15 23:02:48 -0700

    Sort in Rec Centers page works to move names of facility and pictures
    Issue right now is the review databse display doesn't move to follow
    each name of facility/picture.

--- client/src/RecCenters.js
+++ client/src/RecCenters.js
@@ -18,48 +18,120 @@ function RecCenters() {
   const [hitchAverage, setHitchAverage] = useState(0);
   const [sunsetAverage, setSunsetAverage] = useState(0);
   const [BFitAverage, setBFitAverage] = useState(0);
-  const [sortedWithStrings, setSortedWithStrings] = useState([]);
+  const [sortedNames, setSortedNames] = useState([]);
+  const [sortedAspect, setSortedAspect] = useState("");
 
-  function getAverages(props) {
-    setSortedWithStrings(sortedWithStrings=>[]);
-    retrieveAverages("JWCReviews", props).then((averageValue) => {
-      setJWCAverage(averageValue);
-    });
-    retrieveAverages("IMFieldReviews", props).then((averageValue) => {
-      setIMAverage(averageValue);
-    });
-    retrieveAverages("HitchBBReviews", props).then((averageValue) => {
-      setHitchAverage(averageValue);
-    });
-    retrieveAverages("SunsetRecReviews", props).then((averageValue) => {
-      setSunsetAverage(averageValue);
-    });
-    retrieveAverages("BFITReviews", props).then((averageValue) => {
-      setBFitAverage(averageValue);
-    });
-
+  useEffect(() => {
     let foundJWC, foundIM, foundHitch, foundSunset, foundBFit = false;
+    setSortedNames(prevArray=>[]);
     let sortedNums = [JWCAverage, IMAverage, hitchAverage, sunsetAverage, BFitAverage].sort((a, b) => b - a);
+    console.log("JWC " + JWCAverage);
+    console.log("IM " + IMAverage);
+    console.log("hitch " + hitchAverage);
+    console.log("sunset " + sunsetAverage);
+    console.log("bfit " + BFitAverage);
+    
     for(let i = 0; i < sortedNums.length; i++){
       if(sortedNums[i] === JWCAverage && !foundJWC){
         foundJWC = true;
-        setSortedWithStrings(sortedWithStrings=>[sortedWithStrings, "John Wooden Center: " + JWCAverage.toString() + " "]);
+        setSortedNames(prevArray=>[...prevArray, displayJWC]);
       }else if(sortedNums[i] === IMAverage && !foundIM){
         foundIM = true;
-        setSortedWithStrings(sortedWithStrings=>[sortedWithStrings, "Intramural Fields: " + IMAverage.toString()+ " "]);
+        setSortedNames(prevArray=>[...prevArray, displayIM]);
       }else if(sortedNums[i] === hitchAverage && !foundHitch){
         foundHitch = true;
-        setSortedWithStrings(sortedWithStrings=>[sortedWithStrings, "Hitch Basetball Courts: " + hitchAverage.toString()+ " "]);        
+        setSortedNames(prevArray=>[...prevArray, displayHitch]);        
       }else if(sortedNums[i] === sunsetAverage && !foundSunset){
         foundSunset = true;
-        setSortedWithStrings(sortedWithStrings=>[sortedWithStrings, "Sunset Rec: " + sunsetAverage.toString()+ " "]);  
+        setSortedNames(prevArray=>[...prevArray, displaySunset]);  
       }else if(sortedNums[i] === BFitAverage && !foundBFit){
         foundBFit = true;
-        setSortedWithStrings(sortedWithStrings=>[sortedWithStrings, "Bfit: " + BFitAverage.toString()+ " "]); 
+        setSortedNames(prevArray=>[...prevArray, displayBFit]); 
       }
     }
+  }, [sortedAspect]);
 
+  async function getAverages(props) {
+    const jwcAveragePromise = retrieveAverages("JWCReviews", props);
+    console.log(retrieveAverages("JWCReviews", props));
+    const imAveragePromise = retrieveAverages("IMFieldReviews", props);
+    const hitchAveragePromise = retrieveAverages("HitchBBReviews", props);
+    const sunsetAveragePromise = retrieveAverages("SunsetRecReviews", props);
+    const bfitAveragePromise = retrieveAverages("BFITReviews", props);
+  
+    setJWCAverage(await jwcAveragePromise);
+    setIMAverage(await imAveragePromise);
+    setHitchAverage(await hitchAveragePromise);
+    setSunsetAverage(await sunsetAveragePromise);
+    setBFitAverage(await bfitAveragePromise);
+
+    setSortedAspect(props)
     setShowSortedResults(true);
+  }
+
+  function displayJWC(){
+    return (
+      <div>
+                <h3>John Wooden Center</h3>
+        <img src="https://pbs.twimg.com/media/CgMViMxUIAAIU-p.jpg:large"  width="250" height="200" class="JWC"></img>
+        <div class="ListOfReviews">
+          <h3>Reviews:</h3><br></br>
+        {ReviewDatabase("JWCReviews")}
+        </div>
+        </div>
+    );
+  }
+
+  function displayBFit(){
+    return (
+      <div>
+      <h3>Bruin Fitness Center (Bfit)</h3>
+      <img src="https://conferences.ucla.edu/wp-content/uploads/2019/09/bfit.jpg"  width="250" height="200" class="BFIT"></img>
+      <div class="ListOfReviews">
+        <h3>Reviews:</h3><br></br>
+      {ReviewDatabase("BFITReviews")}
+      </div>
+      </div>
+    );
+  }
+
+  function displaySunset(){
+    return (
+      <div>
+        <h3>Sunset Canyon Recreation Center (Sunset Rec)</h3>
+        <img src="https://recreation.ucla.edu/sites/default/files/styles/resize_3_2/public/2022-03/facilities_pools_600x400_1.jpg?itok=97qpWsPL"  width="250" height="200" class="SUNSETREC"></img>
+        <div class="ListOfReviews">
+          <h3>Reviews:</h3><br></br>
+        {ReviewDatabase("SunsetRecReviews")}
+        </div>
+      </div>
+    );
+  }
+
+  function displayHitch(){
+    return (
+      <div>
+        <h3>Hitch Basketball Courts</h3>
+        <img src="https://i.pinimg.com/originals/97/d5/dc/97d5dc01f8ae8694c1e8c319ee3bbf00.png"  width="250" height="200" class="HITCHBB"></img>
+        <div class="ListOfReviews">
+          <h3>Reviews:</h3><br></br>
+        {ReviewDatabase("HitchBBReviews")}
+        </div>
+      </div>
+    );
+  }
+
+  function displayIM(){
+    return (
+      <div>
+        <h3>Intramural Field</h3>
+        <img src="https://recreation.ucla.edu/sites/default/files/styles/header_image/public/2022-03/facilities_IMfield_1156x420.jpg?itok=9NBBHMNs"  width="250" height="200" class="IMFIELD"></img>
+        <div class="ListOfReviews">
+          <h3>Reviews:</h3><br></br>
+        {ReviewDatabase("IMFieldReviews")}
+        </div>
+      </div>
+    );
   }
 
     return (
@@ -80,73 +152,54 @@ function RecCenters() {
     </ul>
     <br></br>
 
-    <div class="SortedResults">  {showSortedResults ? (
+    {/* <div class="SortedResults">  {showSortedResults ? (
     <div>
-      {sortedWithStrings.map((result) => (
+      {sortedNames.map((result) => (
         <p key={result}>{result}<br /></p>
       ))}
     </div>
-  ) : null}</div><br /><br />
-
-        <h3>John Wooden Center</h3>
-        <img src="https://pbs.twimg.com/media/CgMViMxUIAAIU-p.jpg:large"  width="250" height="200" class="JWC"></img>
-        <div class="ListOfReviews">
-          <h3>Reviews:</h3><br></br>
-        {ReviewDatabase("JWCReviews")}
-        </div>
-        <br />
-    <br></br>
-        <h3>Bruin Fitness Center (Bfit)</h3>
-        <img src="https://conferences.ucla.edu/wp-content/uploads/2019/09/bfit.jpg"  width="250" height="200" class="BFIT"></img>
-        <div class="ListOfReviews">
-          <h3>Reviews:</h3><br></br>
-        {ReviewDatabase("BFITReviews")}
-        </div>
-        <br />
-    <br></br>
-        <h3>Sunset Canyon Recreation Center (Sunset Rec)</h3>
-        <img src="https://recreation.ucla.edu/sites/default/files/styles/resize_3_2/public/2022-03/facilities_pools_600x400_1.jpg?itok=97qpWsPL"  width="250" height="200" class="SUNSETREC"></img>
-        <div class="ListOfReviews">
-          <h3>Reviews:</h3><br></br>
-        {ReviewDatabase("SunsetRecReviews")}
-        </div>
-        <br />
-    <br></br>
-        <h3>Hitch Basketball Courts</h3>
-        <img src="https://i.pinimg.com/originals/97/d5/dc/97d5dc01f8ae8694c1e8c319ee3bbf00.png"  width="250" height="200" class="HITCHBB"></img>
-        <div class="ListOfReviews">
-          <h3>Reviews:</h3><br></br>
-        {ReviewDatabase("HitchBBReviews")}
-        </div>
-        <br />
-    <br></br>
-        <h3>Intramural Field</h3>
-        <img src="https://recreation.ucla.edu/sites/default/files/styles/header_image/public/2022-03/facilities_IMfield_1156x420.jpg?itok=9NBBHMNs"  width="250" height="200" class="IMFIELD"></img>
-        <div class="ListOfReviews">
-          <h3>Reviews:</h3><br></br>
-        {ReviewDatabase("IMFieldReviews")}
-
-        </div>
-        <br />
+  ) : null}</div><br /><br /> */}
+  {showSortedResults ? 
+  (<div>
+          {sortedNames[0]()}
+          <br />
+          <br />
+          {sortedNames[1]()}
+          <br />
+          <br />
+          {sortedNames[2]()}
+          <br />
+          <br />
+          {sortedNames[3]()}
+          <br />
+          <br />
+          {sortedNames[4]()}
+          <br />
+  </div>) : 
+    (<div>
+      {displayBFit()}
+      <br />
+      <br />
+      {displayHitch()}
+      <br />
+      <br />
+      {displayIM()}
+      <br />
+      <br />
+      {displayJWC()}
+      <br />
+      <br />
+      {displaySunset()}
+      <br />
+</div>)
+}
 </body>
 </html>
     );
 }
 
 async function retrieveAverages(facilityName, category){
-  let aspect = "";
-  if(category === 1){
-    aspect = "facility";
-  }else if(category === 2){
-    aspect = "hours";
-  }else if(category === 3){
-    aspect = "space";
-  }else if(category === 4){
-    aspect = "location";
-  }else{//category === 5
-    aspect = "activity level";
-  }
-  let averageValue = await computeAverage(facilityName, aspect);
+  let averageValue = await computeAverage(facilityName, category);
   return averageValue;
   // do something with hedrickMatches
 }
@@ -160,67 +213,59 @@ const computeAverage = async(collectionName, category) => {
   const readInReviews = await readInData(reviewCollectionRef); //read in data from review
 
   const length = readInReviews.length; //number of reviews
-  
-  let avgRating = 0;
+
 
   //don't need to do any read in data
   if (length == 0){
-    return avgRating;
+    return 0;
   }
 
   //read in data from collection that string specifies 
-  if (category == "facility"){
+  if (category == "1"){
     //compute average of facility quality 
     let totalRating = 0;
 
     readInReviews.forEach((review) =>{
       totalRating += parseInt(review.FacilityQRating); //add up facility rating for each review
     });
-
-    avgRating = totalRating / length;
+    return totalRating / length;
   }
 
-  if (category == "hours") {
+  if (category == "2") {
     let totalRating = 0;
 
     readInReviews.forEach((review) => {
       totalRating += parseInt(review.HoursRating); //add up facility rating for each review
     });
-
-    avgRating = totalRating / length;
+    return totalRating / length;
   }
 
-  if (category == "space"){
+  if (category == "3"){
     let totalRating = 0;
 
     readInReviews.forEach((review) =>{
       totalRating += parseInt(review.SpaceRating); //add up facility rating for each review
     });
-
-    avgRating = totalRating / length;
+    return totalRating / length;
   }
 
-  if (category == "location"){
+  if (category == "4"){
     let totalRating = 0;
 
     readInReviews.forEach((review) =>{
       totalRating += parseInt(review.LocationRating); //add up facility rating for each review
     });
-
-    avgRating = totalRating / length;
+    return totalRating / length;
   }
 
-  if (category == "activity level"){
+  if (category == "5"){
     let totalRating = 0;
 
     readInReviews.forEach((review) =>{
       totalRating += parseInt(review.BusinessRating); //add up facility rating for each review
     });
-
-    avgRating = totalRating / length;
+    return totalRating / length;
   }
-
-  return avgRating;
   }
   
   //take snapshot and read in data from backend 
@@ -248,7 +293,6 @@ function ReviewDatabase(string){
     const [SpaceRating, setSpaceRating] = useState(-1); //this will be the inout rating from the user
     const [LocationRating, setLocationRating] = useState(-1); //this will be the inout rating from the user
     const [BusinessRating, setBusinessRating] = useState(-1); //this will be the inout rating from the user
-    
     
     const [reviews, setReview] = useState([]);
     const reviewCollectionRef = collection(db, string)
